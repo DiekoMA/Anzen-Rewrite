00fd33a1aa627a97047d361d6934da24070b42dc
Diekoloreoluwa Ujiroghene Akinsete <dj_blueewastaken@outlook.com>
2026-07-31 09:28:07 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Anzen;
+
+public class Program
+{
+    public static async Task StartAnzenServer() {
+        var builder = WebApplication.CreateBuilder();
+        builder.WebHost.UseUrls("http://+:5050");
+    }
+    public static void Task(string[] args)
+    {
+        var command = args[0];
+        var arguments = args[1];
+
+        Console.WriteLine("Anzen Password Manager");
+        Console.WriteLine("Starting API...");
+        Console.WriteLine("Running on port 5050");
+
+
+    }
+}
